87b83e05da5bf9af929cbeabb0162affc9a7a85b
dude-kor <duyong92@gmail.com>
2024-11-19 23:12:30 +0900

    [코딩테스트][무인도 여행] README 및 풀이 시작

---
+++ src/tripToDesertIsland/Main.java
@@ -0,0 +1,10 @@
+package tripToDesertIsland;
+
+public class Main {
+	final static Solution241119 solution241119 =  new Solution241119();
+	
+	public static void main(String[] args) {
+		String[] maps = {"X591X","X1X5X","X231X", "1XXX1"};
+		System.out.println(solution241119.solution(maps));
+	}
+}
---
+++ src/tripToDesertIsland/Solution241119.java
@@ -0,0 +1,10 @@
+package tripToDesertIsland;
+
+public class Solution241119 {
+
+	public int[] solution(String[] maps) {
+		int[] answer = {};
+		return answer;
+	}
+
+}
