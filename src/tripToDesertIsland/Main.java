4f4ec9a9154e66d943eaa701fac91412519ff097
dude-kor <duyong92@gmail.com>
2024-11-21 23:23:47 +0900

    [코딩테스트][무인도 여행] 참고 답안 테스트

--- src/tripToDesertIsland/Main.java
+++ src/tripToDesertIsland/Main.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 public class Main {
 //	final static Solution241119 solution241119 =  new Solution241119();
 	final static Solution241120 solution241120 =  new Solution241120();
+	final static Solution solution = new Solution();
 	
 	public static void main(String[] args) {
 		String[] maps = {"X591X","X1X5X","X231X", "1XXX1"};
-		System.out.println(Arrays.toString(solution241120.solution(maps)));
+//		System.out.println(Arrays.toString(solution241120.solution(maps)));
+		System.out.println(Arrays.toString(solution.solution(maps)));
 	}
 }
